567764bebca699a98d4c841d6406f2926a888890
setug <setu.gupta.study@gmail.com>
2021-11-05 19:36:09 -0400

    Initial base for hedge-12

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+class hedeg_12:
+    def
+
+def welcome(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(name)  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    welcome('This is program to monitor HEDGE-12 from NES')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
